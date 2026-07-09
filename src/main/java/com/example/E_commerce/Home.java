0c679572c5d41306067b77c7a5f66f8b4454fb9f
lazyskiddie <sandeepsingh08550@gmail.com>
2026-07-09 17:45:25 +0530

    Added controller and some page annotations

--- src/main/java/com/example/E_commerce/Home.java
+++ src/main/java/com/example/E_commerce/Home.java
@@ -1,8 +1,13 @@
 package com.example.E_commerce;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Home {
 
+    @GetMapping(path = {"index","home"})
     public String home(){
-        return "Home";
+        return "index";
     }
 }
