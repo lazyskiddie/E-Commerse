e7ae174a791b83567fb220a9cb30f1f79eb933c6
lazyskiddie <sandeepsingh08550@gmail.com>
2026-07-15 20:38:00 +0530

    save and DB list service

--- src/main/java/com/example/E_commerce/Admin/Service/CoffeeService.java
+++ src/main/java/com/example/E_commerce/Admin/Service/CoffeeService.java
@@ -2,7 +2,13 @@ package com.example.E_commerce.Admin.Service;
 
 import com.example.E_commerce.Coffee;
 
+import java.util.List;
+
 public interface CoffeeService {
 
     Coffee saveCoffee(Coffee coffee);
+
+    Coffee saveCoffee(Coffee coffee, String originalName);
+
+    List<Coffee> readService();
 }
