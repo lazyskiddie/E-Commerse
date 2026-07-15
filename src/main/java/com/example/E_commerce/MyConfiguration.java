483aea1dcbbbe0b46bc1b91a30ec6c0553c34bdd
lazyskiddie <sandeepsingh08550@gmail.com>
2026-07-15 20:35:43 +0530

    added model to carry or bring data from DB

--- src/main/java/com/example/E_commerce/MyConfiguration.java
+++ src/main/java/com/example/E_commerce/MyConfiguration.java
@@ -1,13 +1,20 @@
 package com.example.E_commerce;
 
+import org.springframework.ui.Model;
+import com.example.E_commerce.Admin.Service.CoffeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyConfiguration {
 
+    @Autowired
+    private CoffeeService coffeeService;
+
     @GetMapping(path = {"/index", "home"})
-    public String home(){
+    public String home(Model model) {
+        model.addAttribute("listOfService", coffeeService.readService());
         return "index";
     }
 
@@ -22,12 +29,14 @@ public class MyConfiguration {
     }
 
     @GetMapping("/product")
-    public String product(){
+    public String product(Model model){
+        model.addAttribute("listOfService", coffeeService.readService());
         return "product";
     }
 
     @GetMapping("/products")
-    public String products() {
+    public String products(Model model) {
+        model.addAttribute("listOfService", coffeeService.readService());
         return "products";
     }
 }
