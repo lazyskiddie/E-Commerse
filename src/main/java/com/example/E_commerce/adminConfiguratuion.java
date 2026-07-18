0e59b0c45761a40545ad005b889d4bdc5aba8325
lazyskiddie <sandeepsingh08550@gmail.com>
2026-07-18 17:17:06 +0530

    delete coffrr from admin pannel

--- src/main/java/com/example/E_commerce/adminConfiguratuion.java
+++ src/main/java/com/example/E_commerce/adminConfiguratuion.java
@@ -1,13 +1,11 @@
 package com.example.E_commerce;
 
 import com.example.E_commerce.Admin.Service.CoffeeService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -63,5 +61,14 @@ public class adminConfiguratuion {
     public String readService(Model model){
         model.addAttribute("listOfServices", coffeeService.readService());
         return "admin/readService";
+    }
+
+    @GetMapping("/deleteCoffee")
+    public String deleteCoffee(@RequestParam int id, @RequestParam String imageFileName,
+                               RedirectAttributes redirectAttributes, HttpServletRequest request) {
+        String realPath = request.getSession().getServletContext().getRealPath("/src/main/resources/static/images/Services");
+        coffeeService.deleteCoffee(realPath, id, imageFileName);
+        redirectAttributes.addFlashAttribute("message", "Coffee deleted successfully");
+        return "redirect:/admin/readService";
     }
 }
