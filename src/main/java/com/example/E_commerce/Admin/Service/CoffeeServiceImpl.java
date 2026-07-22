ac0c6d72074ae6847e2d92a34b3c0e030a7d95c5
lazyskiddie <sandeepsingh08550@gmail.com>
2026-07-22 19:01:16 +0530

    delete path

--- src/main/java/com/example/E_commerce/Admin/Service/CoffeeServiceImpl.java
+++ src/main/java/com/example/E_commerce/Admin/Service/CoffeeServiceImpl.java
@@ -28,4 +28,17 @@ public class CoffeeServiceImpl implements CoffeeService {
     public List<Coffee> readService() {
         return coffeeRepo.findAll();
     }
+
+    @Override
+    public Void deleteCoffee(String realPath, int id, String imageFileName) {
+        try {
+            java.io.File imageFile = new java.io.File(realPath, imageFileName);
+            if (imageFile.exists()) {
+                imageFile.delete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
