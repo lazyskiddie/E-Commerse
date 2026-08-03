c57f3f3ff566f593f37c5df8c2a402e1617a83f1
lazyskiddie <sandeepsingh08550@gmail.com>
2026-08-03 13:09:43 +0530

    entity for the admin and using lombok

--- src/main/java/com/example/E_commerce/AdminEntity.java
+++ src/main/java/com/example/E_commerce/AdminEntity.java
@@ -1,4 +1,19 @@
 package com.example.E_commerce;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "admin credientials")
 public class AdminEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
 }
