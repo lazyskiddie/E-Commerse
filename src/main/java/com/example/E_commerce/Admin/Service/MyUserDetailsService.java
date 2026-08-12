9021561024ec598030445c0cbe1ba2f0e79123a4
lazyskiddie <sandeepsingh08550@gmail.com>
2026-08-12 07:53:13 +0530

    MyUserdetailsServices which implements UserDetailsService which loadUserByUsername

--- src/main/java/com/example/E_commerce/Admin/Service/MyUserDetailsService.java
+++ src/main/java/com/example/E_commerce/Admin/Service/MyUserDetailsService.java
@@ -1,4 +1,39 @@
 package com.example.E_commerce.Admin.Service;
 
-public class MyUserDetailsService {
+import com.example.E_commerce.Admin.Repo.UserRepository;
+import com.example.E_commerce.AdminEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<AdminEntity> adminEntity = userRepository.findByUsername(username);
+        AdminEntity admin = null;
+
+        if (adminEntity.isPresent()) {
+            admin = adminEntity.get();
+        }else  {
+            throw new UsernameNotFoundException(username);
+        }
+
+        UserDetails userDetails = User
+                .builder()
+                .username(admin.getUsername())
+                .password(admin.getPassword())
+                .build();
+        return userDetails;
+    }
 }
