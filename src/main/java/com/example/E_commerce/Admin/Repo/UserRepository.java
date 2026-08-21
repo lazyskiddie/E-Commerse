7e2cadd60a4f5d680c811d4c3970cf71794fe26b
lazyskiddie <sandeepsingh08550@gmail.com>
2026-08-21 20:48:19 +0530

    Findbyusername added

--- src/main/java/com/example/E_commerce/Admin/Repo/UserRepository.java
+++ src/main/java/com/example/E_commerce/Admin/Repo/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.E_commerce.Admin.Repo;
 
-public interface UserRepository {
+import com.example.E_commerce.AdminEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<AdminEntity, Long> {
+    Optional<AdminEntity> findByUsername(String username);
 }
--- src/main/java/com/example/E_commerce/ECommerceApplication.java
+++ src/main/java/com/example/E_commerce/ECommerceApplication.java
@@ -18,8 +18,8 @@ public class ECommerceApplication {
 
         if (userRepo.findByUsername("admin").isEmpty()) {
             AdminEntity admin = new AdminEntity();;
-            admin.setUsername("UserName");
-            admin.setPassword(Encoder.encode("Password"));
+            admin.setUsername("admin");
+            admin.setPassword(Encoder.encode("admin123"));
             userRepo.save(admin);
             System.out.println("Admin has been saved successfully");
         }else  {
